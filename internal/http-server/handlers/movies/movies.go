e437b416266889349b3460aef4b2f3966c5b0354
Ksenia <k.bashkova@corp.lichi.com>
2026-04-14 22:23:01 +0300

    * Add OpenAi llm to summarize movie review * Add saving movie review to database

--- internal/http-server/handlers/movies/movies.go
+++ internal/http-server/handlers/movies/movies.go
@@ -2,7 +2,11 @@ package movies
 
 import (
 	"context"
+	"errors"
+	"log"
 	"net/http"
+	"os"
+	"strings"
 	"time"
 
 	"streaming/internal/domain/models"
@@ -10,6 +14,8 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"github.com/joho/godotenv"
+	"github.com/tmc/langchaingo/llms/openai"
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
@@ -90,6 +96,9 @@ func AddMovie(client *mongo.Client) gin.HandlerFunc {
 
 func ReviewUpdate(client *mongo.Client) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(c, 100*time.Second)
+		defer cancel()
+
 		movieId := c.Param("imdb_id")
 		if movieId == "" {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "Movie id is required"})
@@ -121,9 +130,109 @@ func ReviewUpdate(client *mongo.Client) gin.HandlerFunc {
 			return
 		}
 
+		filter := bson.D{{Key: "imdb_id", Value: movieId}}
+
+		update := bson.M{
+			"$set": bson.M{
+				"review": req.Review,
+				"ranking": bson.M{
+					"ranking_name":  sentiment,
+					"ranking_value": rankVal,
+				},
+			},
+		}
+
+		movieCollection := mongoDb.OpenCollection(client, "movies")
+
+		result, err := movieCollection.UpdateOne(ctx, filter, update)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error updating movie review"})
+			return
+		}
+
+		if result.MatchedCount == 0 {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Movie not found"})
+			return
+		}
+
+		resp.RankingName = sentiment
+		resp.Review = req.Review
+
+		c.JSON(http.StatusOK, resp)
 	}
 }
 
 func GetReviewRanking(review string, client *mongo.Client, c *gin.Context) (string, int, error) {
-	return "", 0, nil
+	rankings, err := GetRankings(client)
+	if err != nil {
+		return "", 0, err
+	}
+
+	sentimentDelimited := ""
+
+	for _, ranking := range rankings {
+		if ranking.RankingValue != 999 {
+			sentimentDelimited += ranking.RankingName + ","
+		}
+	}
+
+	sentimentDelimited = strings.Trim(sentimentDelimited, ",")
+
+	err = godotenv.Load("internal/config/.env")
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+
+	openAiApiKey := os.Getenv("OPEN_AI_API_KEY")
+	if openAiApiKey == "" {
+		return "", 0, errors.New("could not read OPEN_AI_API_KEY")
+	}
+
+	llm, err := openai.New(openai.WithToken(openAiApiKey))
+	if err != nil {
+		return "", 0, err
+	}
+
+	basePromptTemp := os.Getenv("BASE_PROMPT_TEMP")
+	if basePromptTemp == "" {
+		return "", 0, errors.New("prompt could not be empty")
+	}
+
+	basePrompt := strings.Replace(basePromptTemp, "{rankings}", sentimentDelimited, 1)
+
+	response, err := llm.Call(c, basePrompt+review)
+	if err != nil {
+		return "", 0, err
+	}
+
+	rankVal := 0
+
+	for _, ranking := range rankings {
+		if ranking.RankingName == response {
+			rankVal = ranking.RankingValue
+			break
+		}
+	}
+
+	return response, rankVal, nil
+}
+
+func GetRankings(client *mongo.Client) ([]models.Ranking, error) {
+	var rankings []models.Ranking
+
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	rankingCollection := mongoDb.OpenCollection(client, "ranking")
+	cursor, err := rankingCollection.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+
+	defer cursor.Close(ctx)
+	if err := cursor.All(ctx, &rankings); err != nil {
+		return nil, err
+	}
+
+	return rankings, nil
 }
--- internal/http-server/routes/protected.go
+++ internal/http-server/routes/protected.go
@@ -13,5 +13,6 @@ func SetupProtectedRoutes(router *gin.Engine, client *mongo.Client) {
 
 	router.GET("/movie/:imdb_id", movie_controller.GetMovieById(client))
 	router.POST("/movie", movie_controller.AddMovie(client))
+	router.PATCH("/movie/review/update/:imdb_id", movie_controller.ReviewUpdate(client))
 
 }
